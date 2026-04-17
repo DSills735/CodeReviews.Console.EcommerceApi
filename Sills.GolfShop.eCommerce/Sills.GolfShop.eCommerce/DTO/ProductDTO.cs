848301faf8bd92b8fd16a09a4f2d4ccf8df17f96
DSills735 <David.sills100@gmail.com>
2026-04-17 08:14:00 -0500

    first commit

---
+++ Sills.GolfShop.eCommerce/Sills.GolfShop.eCommerce/DTO/ProductDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace Sills.GolfShop.eCommerceAPI.DTO
+{
+    public record ProductUpdateDto
+    {
+        public string Name { get; init; }
+        public string Description { get; init; }
+        public int QuantityInStock { get; init; }
+    }
+}
